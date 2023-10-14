abe6675e49342a8236dbbc88be4a451c72c74998
Evgeniy <trofimovevgeniy9@gmail.com>
2023-10-14 17:44:27 +0300

    Кодирование блока

--- lab4/coder.py
+++ lab4/coder.py
@@ -16,15 +16,23 @@ class Coder(object):
             encoded_block = self._encode_block(block)
         return ''
 
-    def _encode_block(self, block: List[int]):
+    def _encode_block(self, block: List[int]) -> List[int]:
         """Кодирование блока"""
         block = self._get_initial_block_permutation(block)
 
+        left_part, right_part = block[0:32], block[32:63]
         for i in range(16):
             # 16 циклов шифрующих преобразований
-            pass
+            new_left_part = right_part
+            key = self.keys[i]
+            new_right_part = self._lists_xor(left_part, self._func_r_k(right_part, key))
 
+            left_part = new_left_part
+            right_part = new_right_part
+
+        block = left_part + right_part
         block = self._get_reverse_block_permutation(block)
+        return block
 
     def _get_initial_block_permutation(self, block: List[int]) -> List[int]:
         """Начальная перестановка"""
@@ -48,11 +56,30 @@ class Coder(object):
         ]
         return [block[ind - 1] for ind in key]
 
-    def _func_r_k(self, r_part_block: List[int]):
+    def _func_r_k(self, r_part_block: List[int], key: List[int]):
         """f(R[i-1], k[i])"""
-        extended_block = self._extend_block(r_part_block)
+        # Расширение до 48 битов
+        block = self._extend_block(r_part_block)
+        # Гаммирование с ключом
+        block = self._lists_xor(r_part_block, key)
+        # Разбиение на 8 блоков по 6 бит
+        parts = [block[i:i + 6] for i in range(0, len(block), 6)]
 
-    def _extend_block(self, r_part_block: List[int]):
+        # Новый 32-битный полублок
+        block = []
+        for ind, part in enumerate(parts):
+            part_value = self._ss_transformation(ind, part)
+            block.extend(part_value)
+        return self._get_block_permutation(block)
+
+    def _get_block_permutation(self, block: List[int]):
+        permutation_key = [
+            16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5, 18, 31, 10,
+            2, 8, 24, 14, 32, 27, 3, 9, 19, 13, 30, 6, 22, 11, 4, 25
+        ]
+        return [block[ind - 1] for ind in permutation_key]
+
+    def _extend_block(self, r_part_block: List[int]) -> List[int]:
         """Расширение 32-битного блока до 48 бит"""
         key = [
             [2, 48], [3], [4], [5, 7], [6, 8], [9], [10], [11, 13], [12, 14], [15], [16], [17, 19], [18, 20], [21],
@@ -66,6 +93,67 @@ class Coder(object):
             for pos in positions:
                 result[pos] = bit
         return result
+
+    @staticmethod
+    def _ss_transformation(ind: int, part: List[int]) -> List[int]:
+        """SS-преобразование"""
+        s1 = [
+            [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
+            [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
+            [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
+            [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13],
+        ]
+        s2 = [
+            [15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],
+            [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
+            [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
+            [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9],
+        ]
+        s3 = [
+            [10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
+            [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
+            [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
+            [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12],
+        ]
+        s4 = [
+            [7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
+            [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
+            [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
+            [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14],
+        ]
+        s5 = [
+            [2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
+            [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
+            [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
+            [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3],
+        ]
+        s6 = [
+            [12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
+            [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
+            [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
+            [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13],
+        ]
+        s7 = [
+            [4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
+            [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
+            [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
+            [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12],
+        ]
+        s8 = [
+            [13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
+            [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
+            [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 13, 5, 8],
+            [2, 1, 14, 7, 4, 10, 8, 13, 5, 12, 9, 0, 3, 5, 5, 11],
+        ]
+        ss_key = [
+            s1, s2, s3, s4, s5, s6, s7, s8
+        ]
+        k = int(f'{part[0]}{part[5]}', 2)
+        l_ = int(f'{part[2]}{part[3]}{part[4]}{part[5]}', 2)
+        res_dec = ss_key[ind][k][l_]
+        res_bin = bin(res_dec)[2::]
+        res_bin = res_bin.zfill(4)
+        return list(map(int, res_bin))
 
     # endregion
 
@@ -106,6 +194,14 @@ class Coder(object):
             # Это был последний символ блока, можем вернуть блок
             if ind % block_len == block_len - 1:
                 yield result
+
+    @staticmethod
+    def _lists_xor(list_1: List[int], list_2: List[int]) -> List[int]:
+        """Сложение по модулю 2 элементов списка"""
+        result = []
+        for el_1, el_2 in zip(list_1, list_2):
+            result.append(el_1 ^ el_2)
+        return result
 
     # endregion
 
