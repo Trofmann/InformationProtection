3f12209455863a840393ff9a4fe66c376786110e
Evgeniy <trofimovevgeniy9@gmail.com>
2023-10-14 14:48:36 +0300

    Получение ключей

--- lab4/coder.py
+++ lab4/coder.py
@@ -6,8 +6,9 @@ from utils import get_char_code
 class Coder(object):
 
     def __init__(self, key):
-        self.key = key
-        assert len(self.key) == 8  # Длина ключа должна быть равна 8
+        if len(key) != 8:
+            raise Exception('Длина ключа должна быть равна 8')
+        self.keys = self._get_keys(key)
 
     # region Кодирование
     def encode(self, msg: str) -> str:
@@ -83,7 +84,7 @@ class Coder(object):
         code_str = code_str.zfill(8)
         return list(map(int, code_str))
 
-    def split_text_to_blocks(self, msg) -> Generator[List[int], None, None]:
+    def split_text_to_blocks(self, msg: str) -> Generator[List[int], None, None]:
         """Разбиение текста на блоки по 64 бита"""
         # Заметим, что 1 символ == 1 байт, значит, надо разбить на блоки по 8 символов
         # Для удобства же сразу будем возвращать список битов
@@ -109,16 +110,57 @@ class Coder(object):
     # endregion
 
     # region Ключи
-    def get_keys(self, key) -> List[List[int]]:
+    def _get_keys(self, key_txt: str) -> List[List[int]]:
         """
         Получение ключей
-        :param key: введённый ключ
+        :param key_txt: введённый ключ
         :return:
         """
-        key_in_bit_format = []  # Ключ в виде массива битов
-        keys = []
-        for sym in key:
-            key_in_bit_format.extend(self._get_symbol_byte_list(sym))
+        key = []  # type: List[int]  # Ключ в виде массива битов
+        keys = []  # type: List[List[int]]
+        for sym in key_txt:
+            key.extend(self._get_symbol_byte_list(sym))
+        key = self._get_key_permutation_1(key)
 
-        return []
+        key_left_part, key_right_part = key[0:28], key[28:56]
+
+        for i in range(16):
+            key_left_part = self._key_part_cyclic_left_shift(key_left_part, i)
+            key_right_part = self._key_part_cyclic_left_shift(key_right_part, i)
+            key = key_left_part + key_right_part
+            key = self._get_key_permutation_2(key)
+            keys.append(key)
+
+        return keys
+
+    @staticmethod
+    def _get_key_permutation_1(key: List[int]) -> List[int]:
+        """Перестановка-выбор 1"""
+        permutation_key = [
+            57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18,
+            10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36,
+            63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22,
+            14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4
+        ]
+        return [key[ind - 1] for ind in permutation_key]
+
+    @staticmethod
+    def _get_key_permutation_2(key: List[int]) -> List[int]:
+        """Перестановка-выбор 2"""
+        permutation_key = [
+            14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4,
+            26, 8, 16, 7, 27, 20, 13, 2, 41, 52, 31, 37, 47, 55, 30, 40,
+            51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32
+        ]
+        return [key[ind - 1] for ind in permutation_key]
+
+    @staticmethod
+    def _key_part_cyclic_left_shift(key_part: List[int], ind: int) -> List[int]:
+        """Циклический сдвиг влево части ключа"""
+        shift_key = [
+            1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1
+        ]
+        value = shift_key[ind]
+        return key_part[value:] + key_part[:value]
+
     # endregion
