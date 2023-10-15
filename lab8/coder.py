f1a303740462fe7219522093bbbe5b5eb2349ee9
Evgeniy <trofimovevgeniy9@gmail.com>
2023-10-15 20:37:01 +0300

    Кодирование + тест

--- lab8/coder.py
+++ lab8/coder.py
@@ -1,3 +1,6 @@
+from utils import get_text_bits_list
+
+
 class Coder(object):
     def __init__(self, key: str = ''):
         self.key = key  # Для дешифрования ключ не нужен
@@ -8,9 +11,10 @@ class Coder(object):
             raise Exception('Для шифрования необходим контейнер')
 
         key_len = len(self.key)
+        msg = get_text_bits_list(msg)  # Переводим сообщение в массив бит
         msg_len = len(msg)
         result = []
-        msg_sym_ind = 0  # Индекс элемента, который надо вставить в контейнер
+        msg_sym_ind = 0  # Указатель на бит, который надо вставить в контейнер
 
         for ind, sym in enumerate(self.key):
             # Очевидно, что текущий символ надо добавлять всегда
@@ -21,6 +25,14 @@ class Coder(object):
             if msg_sym_ind == msg_len - 1:
                 # Сообщение полностью в контейнере
                 continue
+
+            # Конец предложение и следующий символ пробел - место для внесения данных
+            if sym in self.sentence_separators and self.key[ind + 1] == ' ':
+                if not msg[msg_sym_ind]:
+                    # Бит равен 0, добавим пробел
+                    result.append(' ')
+                # Сдвинем указатель
+                msg_sym_ind += 1
 
         if msg_sym_ind != msg_len - 1:
             return 'Сообщение не влезло в контейнер'
--- lab8/test.py
+++ lab8/test.py
@@ -5,7 +5,11 @@ from lab8.coder import Coder
 
 class TestLab8Coder(unittest.TestCase):
     def test_open_text_equals_decoded(self):
-        key = '123'
+        key = ''
+        with open('key.txt', encoding='utf-8') as f:
+            key = f.read()
         open_text = '1231414'
         coder = Coder(key)
+        encoded = coder.encode(open_text)
+        print(encoded)
         self.assertEqual(open_text, coder.decode(coder.encode(open_text)))
