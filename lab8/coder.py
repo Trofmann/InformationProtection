f7a7a62ee28995f0c4d9509aaad11b129dd64cef
Evgeniy <trofimovevgeniy9@gmail.com>
2023-10-15 21:27:09 +0300

    декодирование

--- lab8/coder.py
+++ lab8/coder.py
@@ -1,4 +1,6 @@
-from utils import get_text_bits_list
+from typing import List, Generator
+
+from utils import get_text_bits_list, get_bit_list_text
 
 
 class Coder(object):
@@ -20,7 +22,7 @@ class Coder(object):
 
         key_len = len(self.key)
         msg = get_text_bits_list(msg)  # Переводим сообщение в массив бит
-        msg.append(0)  # Последний ноль - маркер  окончания сообщения
+        msg.append(0)  # Последний ноль - маркер окончания сообщения
         msg_len = len(msg)
         result = []
         msg_sym_ind = 0  # Указатель на бит, который надо вставить в контейнер
@@ -31,7 +33,7 @@ class Coder(object):
             if ind == key_len - 1:
                 # Конец контейнера
                 continue
-            if msg_sym_ind == msg_len - 1:
+            if msg_sym_ind == msg_len:
                 # Сообщение полностью в контейнере
                 continue
 
@@ -43,9 +45,42 @@ class Coder(object):
                 # Сдвинем указатель
                 msg_sym_ind += 1
 
-        if msg_sym_ind != msg_len - 1:
+        if msg_sym_ind != msg_len:
             return 'Сообщение не влезло в контейнер'
         return ''.join(result)
 
     def decode(self, msg: str) -> str:
-        return ''
+        msg_len = len(msg)
+        result = []  # type: List[int]
+        result_str = []  # type: List[str]
+        for ind, sym in enumerate(msg):
+            if ind == msg_len - 1:
+                continue
+            if sym in self.sentence_separators:
+                count_ = (msg[ind + 1] == ' ')  # Следующий символ - пробел
+                if count_:
+                    # Рассматриваем, только если следующий символ пробел
+                    count_ += ((ind + 2 <= msg_len - 1) and msg[ind + 2] == ' ')  # Через символ тоже пробел
+                    result.append(count_ % 2)
+
+        # Вспоминаем про маркер окончания сообщения.
+        # Добираем до последнего нуля
+        while result[-1]:
+            result.pop(-1)
+        if result[-1] == 0:
+            # И ноль тоже удаляем, т.к это маркер
+            result.pop(-1)
+
+        for block in self._split_to_blocks(result):
+            block = get_bit_list_text(block)
+            result_str.append(block)
+        return ''.join(result_str)
+
+    def _split_to_blocks(self, list_: List[int], block_len=8) -> Generator[List[int], None, None]:
+        block = []
+        for elem in list_:
+            block.append(elem)
+            if len(block) == block_len:
+                temp = block
+                block = []
+                yield temp
--- lab8/test.py
+++ lab8/test.py
@@ -7,7 +7,7 @@ class TestLab8Coder(unittest.TestCase):
     def test_open_text_equals_decoded(self):
         with open('key.txt', encoding='utf-8') as f:
             key = f.read()
-        open_text = '1231414'
+        open_text = '1231514'
         coder = Coder(key)
         encoded = coder.encode(open_text)
         print(encoded)
