686dafae855114de715461b8383c019c97d44df1
Evgeniy <trofimovevgeniy9@gmail.com>
2023-10-15 20:10:23 +0300

    Заготовка кодирования

--- lab8/coder.py
+++ lab8/coder.py
@@ -6,7 +6,24 @@ class Coder(object):
     def encode(self, msg: str) -> str:
         if not self.key:
             raise Exception('Для шифрования необходим контейнер')
+
+        key_len = len(self.key)
+        msg_len = len(msg)
         result = []
+        msg_sym_ind = 0  # Индекс элемента, который надо вставить в контейнер
+
+        for ind, sym in enumerate(self.key):
+            # Очевидно, что текущий символ надо добавлять всегда
+            result.append(sym)
+            if ind == key_len - 1:
+                # Конец контейнера
+                continue
+            if msg_sym_ind == msg_len - 1:
+                # Сообщение полностью в контейнере
+                continue
+
+        if msg_sym_ind != msg_len - 1:
+            return 'Сообщение не влезло в контейнер'
         return ''.join(result)
 
     def decode(self, msg: str) -> str:
