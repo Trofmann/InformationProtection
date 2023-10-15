9c5693397ff674ce259d8a93e2b1c5041bfe9148
Evgeniy <trofimovevgeniy9@gmail.com>
2023-10-15 13:40:00 +0300

    Декодирование

--- lab5/coder.py
+++ lab5/coder.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from des_mixin import DesMixin
 from gamma_generator import GammaGenerator
 from lab4 import Coder as Lab4Coder
@@ -23,5 +25,21 @@ class Coder(DesMixin):
             result.append(encoded_block)
         return ''.join(result)
 
-    def decode(self, msg: str):
-        pass
+    def decode(self, msg: str) -> str:
+        c = next(GammaGenerator().generate_new(1))
+        # Предыдущий зашифрованный блок
+        prev_encoded_block = self.get_int_bit_list(c, 64)  # type: List[int]
+        # Результат
+        result = []
+        for block in self.split_text_to_blocks(msg):
+            # Mi + 1 = Ci xor DES-1(K, Ci + 1).
+            decoded_block = self.des_coder.decode(block)  # DES-1(K, Ci + 1)
+            decoded_block_bits_list = self.get_text_bits_list(decoded_block)
+            decoded_block = decoded_block_bits_list
+            decoded_block = self.lists_xor(prev_encoded_block, decoded_block)  # Ci xor DES-1(K, Ci + 1)
+            decoded_block = self.get_bit_list_text(decoded_block)  # Mi + 1
+            result.append(decoded_block)
+
+            prev_encoded_block = decoded_block_bits_list
+
+        return ''.join(result)
--- lab5/test.py
+++ lab5/test.py
@@ -4,9 +4,10 @@ from lab5.coder import Coder
 
 class TestLab5Coder(unittest.TestCase):
     def test_working(self):
-        key = 'baabbaaa'
-        open_text = '123456891011121314'
+        key = 'aaaaaaaa'
+        open_text = '123456789101112131415'
         coder = Coder(key=key)
         encoded = coder.encode(open_text)
         print(encoded)
-        self.assertEqual(True, True)
+        self.assertEqual(open_text, coder.decode(coder.encode(open_text)).rstrip(' '))
+
