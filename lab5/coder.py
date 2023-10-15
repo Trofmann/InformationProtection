d2f1ae3e6c674f26c0bca210ba8f80fe3007ec54
Evgeniy <trofimovevgeniy9@gmail.com>
2023-10-15 11:12:47 +0300

    Подготовка к ЛБ 5

--- lab5/coder.py
+++ lab5/coder.py
@@ -1,3 +1,6 @@
+from gamma_generator import GammaGenerator
+
+
 class Coder(object):
     def __init__(self, key: str):
         self.key = key
@@ -6,4 +9,8 @@ class Coder(object):
         pass
 
     def decode(self, msg: str):
+        pass
+
+    def _split_text_to_blocks(self, text: str):
+        # Надо разбить текст на 64 битовые блоки. 1 символ 8 бит, значит на блоки по 8 символов
         pass
