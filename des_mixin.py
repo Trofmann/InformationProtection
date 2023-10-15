407da6cbbf39c732fc94eaddaa3b7174e9a1ffbc
Evgeniy <trofimovevgeniy9@gmail.com>
2023-10-15 20:22:29 +0300

    Вынес в утилсы, потому что надо в другой лабе

--- des_mixin.py
+++ des_mixin.py
@@ -1,7 +1,9 @@
 from typing import List, Generator
 
-from utils import get_char_code, split_to_blocks, get_code_char
 from gamma_generator import GammaGenerator
+from utils import (
+    get_int_bit_list, get_symbol_bit_list, get_text_bits_list, get_bit_list_text
+)
 
 
 class DesMixin(object):
@@ -10,14 +12,7 @@ class DesMixin(object):
     @staticmethod
     def get_symbol_bit_list(symbol: str) -> List[int]:
         """Преобразование символа в массив битов"""
-        code = get_char_code(symbol)
-        return DesMixin.get_int_bit_list(code, len_=8)
-
-    @staticmethod
-    def get_int_bit_list(num: int, len_=8) -> List[int]:
-        num_str = bin(num)[2::]
-        num_str = num_str.zfill(len_)
-        return list(map(int, num_str))
+        return get_symbol_bit_list(symbol)
 
     def split_text_to_blocks(self, msg: str) -> Generator[str, None, None]:
         """Разбиение текста на блоки по 64 бита"""
@@ -43,10 +38,7 @@ class DesMixin(object):
 
     def get_text_bits_list(self, text: str) -> List[int]:
         """Текст в список битов"""
-        result = []
-        for sym in text:
-            result.extend(self.get_symbol_bit_list(sym))
-        return result
+        return get_text_bits_list(text)
 
     @staticmethod
     def lists_xor(list_1: List[int], list_2: List[int]) -> List[int]:
@@ -59,16 +51,10 @@ class DesMixin(object):
     @staticmethod
     def get_bit_list_text(list_: List[int]) -> str:
         """Перевод списка бит в строку"""
-        parts = split_to_blocks(list_, 8)
-        result = []
-        for part in parts:
-            bin_str = ''.join(map(str, part))
-            symbol_code = int(bin_str, 2)
-            result.append(get_code_char(symbol_code))
-        return ''.join(result)
+        return get_bit_list_text(list_)
 
     def get_initial_gamma(self, elem_count: int, elem_len: int) -> List[int]:
         result = []
         for num in GammaGenerator().generate_new(elem_count):
-            result.extend(self.get_int_bit_list(num, elem_len))
+            result.extend(get_int_bit_list(num, elem_len))
         return result
--- utils.py
+++ utils.py
@@ -13,3 +13,35 @@ def get_char_code(c: str) -> int:
 
 def get_code_char(code: int) -> str:
     return const.alphabet[code]
+
+
+def get_int_bit_list(num: int, len_=8) -> List[int]:
+    """Преобразование числа в список бит"""
+    num_str = bin(num)[2::]
+    num_str = num_str.zfill(len_)
+    return list(map(int, num_str))
+
+
+def get_symbol_bit_list(symbol: str) -> List[int]:
+    """Преобразование символа в массив битов"""
+    code = get_char_code(symbol)
+    return get_int_bit_list(code, len_=8)
+
+
+def get_text_bits_list(text: str) -> List[int]:
+    """Текст в список битов"""
+    result = []
+    for sym in text:
+        result.extend(get_symbol_bit_list(sym))
+    return result
+
+
+def get_bit_list_text(list_: List[int]) -> str:
+    """Перевод списка бит в строку"""
+    parts = split_to_blocks(list_, 8)
+    result = []
+    for part in parts:
+        bin_str = ''.join(map(str, part))
+        symbol_code = int(bin_str, 2)
+        result.append(get_code_char(symbol_code))
+    return ''.join(result)
