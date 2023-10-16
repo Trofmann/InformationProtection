c765f534bcb15c3a58c2806606726264e53f0943
Evgeniy <trofimovevgeniy9@gmail.com>
2023-10-16 19:16:22 +0300

    Декодирование

--- lab9_1/coder.py
+++ lab9_1/coder.py
@@ -1,6 +1,6 @@
-from typing import List
+from typing import List, Generator
 
-from utils import get_text_bits_list
+from utils import get_text_bits_list, get_bit_list_text
 
 
 class Coder(object):
@@ -37,4 +37,28 @@ class Coder(object):
         return self.separator.join(result)
 
     def decode(self, msg: str) -> str:
-        return ''
+        msg_len = len(msg)
+        result = []  # type: List[int]
+        result_str = []  # type: List[str]
+
+        for line in msg.split(self.separator):
+            # Строка не заканчивается на пробел, а значит больше нет строк с пробелами
+            if line[-1] != ' ':
+                break
+            count_ = (line[-1] == ' ') + (line[-2] == ' ')
+            # 0 Кодируется 2 пробелами, 1 - 1 пробелом
+            result.append(count_ % 2)
+
+        for block in self._split_to_blocks(result):
+            block = get_bit_list_text(block)
+            result_str.append(block)
+        return self.separator.join(result_str)
+
+    def _split_to_blocks(self, list_: List[int], block_len=8) -> Generator[List[int], None, None]:
+        block = []
+        for elem in list_:
+            block.append(elem)
+            if len(block) == block_len:
+                temp = block
+                block = []
+                yield temp
